2993ad83c5aaf7b2fdf6cc53b2eef852b3db898e
TruptiPowar <trupticpowar@gmail.com>
2023-05-02 23:29:21 +0530

    Constructor Changes

---
+++ Car.java
@@ -0,0 +1,16 @@
+class Car
+{
+	String model;
+	String color;
+	boolean available;
+	int quantity;
+	
+	public Car()
+	{
+		this.model=model;
+		this.color=color;
+		this.available=available;
+		this.quantity=quantity;
+	}
+
+}
